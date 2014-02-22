35d36e4942cdbcc6af17e049783c7e334ffbcd81
Eric Perez <ericricardoperez@gmail.com>
2014-02-21 22:41:18 -0800

    index action for habits. both json and html reponse.

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,2 +1,12 @@
 class HabitsController < ApplicationController
+
+  def index
+    @habits = Habit.all
+
+    respond_to |f|
+      f.html
+      f.json { render json: { @habits.to_json } }
+    end
+  end
+
 end

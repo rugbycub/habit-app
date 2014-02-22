b7ea49cd8442ecc5237ee20a9b711ca34a03cf80
lalanikki <lalanikki@gmail.com>
2014-02-22 14:51:33 -0800

    completed devise-install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ HabitApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #[NIKKI] PER DEVISE Ensure you have defined default url options in your environments files.
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end

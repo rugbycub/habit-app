a2fb02e75dbf75170192c77ca0b24a938ca25325
Charlie Nuttall <CENuttall@mac.com>
2014-02-23 00:35:00 -0800

    added models for entry friendship habit and message UNTESTED will test in the morning

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222234028) do
+ActiveRecord::Schema.define(version: 20140223035736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: true do |t|
+    t.boolean  "status"
+    t.integer  "habit_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "habits", force: true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.integer  "frequency"
+    t.string   "timeframe"
+    t.string   "visibility"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: true do |t|
+    t.integer  "message_from_user_id"
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

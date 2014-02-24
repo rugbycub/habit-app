b36240e1dd0ba51833d4e6802fdf566c3ea8cb8f
lalanikki <lalanikki@gmail.com>
2014-02-23 23:21:07 -0800

    phone,first,last,profile pic included in new registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ HabitApp::Application.configure do
 
   #[NIKKI] PER DEVISE Ensure you have defined default url options in your environments files.
   config.action_mailer.default_url_options = { :host => 'thawing-cove-2646.herokuapp.com' }
+
+  #[NIKKI] Adding paperclip defaults pointing to keys
+  config.paperclip_defaults = {
+  :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end

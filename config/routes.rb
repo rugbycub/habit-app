946f0ccc97a9edda228427ba9c61e89e704347fc
stuartstein <stuart.stein@gmail.com>
2014-02-24 12:47:30 -0800

    Added the logo header to all pages in the application where the user is signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 HabitApp::Application.routes.draw do
   devise_for :users
   #[NIKKI] temporary root per devise
+  get "/habits" => "habits#index", as: 'habits_path'
+
   root :to => "welcome#index"
 
 end

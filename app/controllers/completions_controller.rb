bcf4ed0f9aad8d4de46a2b58a4d822c04be3259c
Eric Perez <ericricardoperez@gmail.com>
2014-02-23 22:34:39 -0800

    create action

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,2 +1,14 @@
 class CompletionsController < ApplicationController
+
+  def create
+    habit = current_user.habits.find_by_name()
+    complete = habit.completions.create()
+
+    respond_to do |f|
+      f.json { render json: complete.to_json }
+    end
+  end
+
+  
+
 end

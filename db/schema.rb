aa94af22dbb83fc674c32d3f12a17a901570bc91
Charlie Nuttall <CENuttall@mac.com>
2014-02-24 16:25:00 -0800

    seed data generating

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224180910) do
+ActiveRecord::Schema.define(version: 20140224234543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "entries", force: true do |t|
+  create_table "completions", force: true do |t|
     t.boolean  "status"
     t.integer  "habit_id"
     t.datetime "created_at"

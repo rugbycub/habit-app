da550fa4e1a796d54a788da907d08fabb5c27ef2
stuartstein <stuart.stein@gmail.com>
2014-02-24 18:06:43 -0800

    Changed order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 HabitApp::Application.routes.draw do
   devise_for :users
   #[NIKKI] temporary root per devise
+  get "/habits/new" => "habits#new", as: 'new_habit' 
   get "/habits/:id" => "habits#show", as: 'habit'
   get "/habits/" => "habits#index", as: 'habits'
-  get "/habits/new" => "habits#new", as: 'new_habit' 
   root :to => "welcome#index"
 
   post "/completions", to: "completions#create"

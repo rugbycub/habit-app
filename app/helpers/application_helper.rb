46a102dad6fbac051d79351fafde5b7085ab08df
lalanikki <lalanikki@gmail.com>
2014-02-24 17:59:07 -0800

    paths only for sign-in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+ #so log-in form is available in layouts/application
+ def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end

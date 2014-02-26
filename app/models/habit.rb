14f17454e9be197119feb12d35627af87611859a
stuartstein <stuart.stein@gmail.com>
2014-02-26 13:45:03 -0800

    Added validation to the new habit form

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -7,10 +7,9 @@ class Habit < ActiveRecord::Base
 
     has_many :points, as: :pointable
 
-    validates :frequency, presence: true
-    validates :frequency, numericality: { only_integer: true }
-    validates :frequency, inclusion: 1..7, if: :weekly?
-    validates :frequency, inclusion: {in: 1..31, message: "ERROR", if: :monthly?} 
+    validates :frequency, presence: { message: "Add your number of days per week for this habit" }
+    validates :frequency, inclusion: {in: 1..7, message: "Enter a number 1 to 7 for the week", if: :weekly?} 
+    validates :frequency, inclusion: {in: 1..31, message: "Enter a number 1 to 31 for the week", if: :monthly?} 
 
     def weekly?
       timeframe == "week"

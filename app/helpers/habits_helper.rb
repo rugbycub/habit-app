d786b54a66a243128ce340ae9ccbc3abb6504790
stuartstein <stuart.stein@gmail.com>
2014-02-26 19:36:27 -0800

    Created posts template

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,21 +1,29 @@
 module HabitsHelper
 
-def get_freq(freq, my_date)
-  month_size = Time.days_in_month(my_date.month, my_date.year)
-  month_size < freq ? month_size : freq
-end
+  def get_freq(freq, my_date)
+    month_size = Time.days_in_month(my_date.month, my_date.year)
+    month_size < freq ? month_size : freq
+  end
 
-def days_of_week
-  ["Su", "M", "Tu", "W", "Th", "F", "Sa"]
-end
+  def days_of_week
+    ["Su", "M", "Tu", "W", "Th", "F", "Sa"]
+  end
 
-def this_sunday
-  Date.today.beginning_of_week - 1
-end
+  def this_sunday
+    Date.today.beginning_of_week - 1
+  end
 
-def is_today(my_date)
-  my_date.cwday == Date.today.cwday &&
-  my_date.cwyear == Date.today.cwyear
-end
+  def is_today(my_date)
+    my_date.cwday == Date.today.cwday &&
+    my_date.cwyear == Date.today.cwyear
+  end
+
+  def show_votes(votes)
+    "(#{votes})" unless votes.nil?
+  end
+
+  def show_date(my_date)
+    my_date.strftime("%-m/%e/%y")
+  end
 
 end

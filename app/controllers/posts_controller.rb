4563efb2b70cdf45942c9f39fd84806f1c23363c
Eric Perez <ericricardoperez@gmail.com>
2014-02-26 21:01:39 -0800

    fixed create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :determine_habit
 
   def create
     user = current_user
-    binding.pry
-    habit = Habit.find(params[:id])
+    habit = Habit.find(params[:habit_id])
     post = habit.posts.create(params[:post].permit(:body))
     post.user_id = user.id
     post.save
 
     respond_to do |f|
-      f.json {render json: {post: post.as_json( include: [ :user ]), profile:  user.profile.as_json } }
+      f.json {render json: {post: post.as_json( include: [ :user ]), profile: user.profile.as_json } }
     end
 
-  end
-
-  def determine_habit
-    binding.pry
   end
 
 end
